88521dda40c9e2a5e5f16eb589a1dfc9241ddc0f
Trismegiste <flo@trismegiste.net>
2017-06-29 19:44:49 +0200

    pk string

--- js/db.js
+++ js/db.js
@@ -1,31 +1,31 @@
 function init() {
     RpgImpro.document.vertex = [
         {
-            pk: 1,
+            pk: '1',
             hashtag: 'scenario',
             sentence: "#templier veut tuer #nephilim"
         },
         {
-            pk: 2,
+            pk: '2',
             hashtag: 'templier',
             sentence: "Manteau noir équipé #artefact"
         },
         {
-            pk: 3,
+            pk: '3',
             hashtag: 'nephilim',
             sentence: "un pj"
         },
         {
-            pk: 4,
+            pk: '4',
             hashtag: 'artefact',
             sentence: "anneau orichalque"
         }
     ]
 
     RpgImpro.document.edge = [
-        {source: 1, target: 2},
-        {source: 1, target: 3},
-        {source: 2, target: 4}
+        {source: '1', target: '2'},
+        {source: '1', target: '3'},
+        {source: '2', target: '4'}
     ]
 
     RpgImpro.repository.vertex = [
